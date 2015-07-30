2a6906795823ef4e6be98ef40e23f0abc8df91a1
dmpost <dmi.post@gmail.com>
2015-07-30 16:27:22 +0300

    hot1812

---
+++ data/hotFix1812.js
@@ -0,0 +1,115 @@
+function onCloseRecentFilesWindow(){
+	inOpenFilesOptionsMode = false;
+	swtrace = 0;
+	createSWiframe();
+	setTimeout(function(){
+		if(!localStorage.welcomTourViewed)startWelcomeTour()
+	},3500);
+}
+
+function getHamburgerTextCode(){
+	var str='';
+	if(menuSettings.hamburgerText){
+		str +="	#swimbi>ul:after {\n"
+		str +="		line-height: "+(menuSettings.mainmenuHeight-2)+"px;\n"
+		str +="		content: '"+menuSettings.hamburgerText+"';\n"
+		str +="		font-family: "+menuSettings.mainFontFamily+" !important;\n"
+		str +="		font-size: "+menuSettings.mainTextSize+"px;\n"
+		str +="		color: #"+menuSettings.mainTextColor+";\n"
+		str +="		float:right;\n"
+		str +="		padding-right: 15px;\n"
+		str +="	}\n\n"
+
+		str +="	#swimbi>ul:hover:after, #swimbi>ul.hvr:after {\n"
+		str +="		display: none;\n"
+		str +="	}\n\n"
+	}
+	return str;
+}
+
+function addResponsiveCode(){
+	if(menuSettings.isResponsive){
+		if(menuSettings.menuLayout.indexOf('vertical')<0){
+			var borderTopSize = getIconSizeForResponsive()*3/8;
+			var borderBottomSize = getIconSizeForResponsive()/8;
+			var hamburgerWidth = (getIconSizeForResponsive()/1.2)|0;
+			var hamburgerHeight = borderBottomSize;
+
+			var hamburgerRestHeight = (menuSettings.mainmenuHeight - hamburgerHeight - borderBottomSize - borderTopSize)/2;
+
+			var topAdd = .5;
+			var topMargin = (hamburgerRestHeight + topAdd)|0;
+			var bottomMargin = menuSettings.mainmenuHeight - hamburgerHeight - borderBottomSize - borderTopSize - topMargin;
+
+
+			var str = "@media (max-width: "+menuSettings.responsiveThreshold+"px) {\n"
+
+				str +="	#swimbi>ul{\n"
+				str +="		min-width: 300px !important;\n"
+				str +="		width: 100%;\n"
+				str +="		height: "+menuSettings.mainmenuHeight+"px;\n"
+				str +="		padding: 0 !important;\n"
+				str +="	}\n\n"
+
+				str +="	#swimbi ul li{\n"
+		        str +="		float: none;\n"
+		        str +="		display: block;\n"
+				str +="		position:relative;\n"
+			    str +="	}\n\n"
+
+			    str +="	#swimbi>ul>li{\n"
+			    str +="		width: auto !important;\n"
+		        str +="		margin: 0 "+responsiveMarginRight+" 0 "+responsiveMarginLeft+";\n"
+			    str +="	}\n\n"
+				
+				str +="	#swimbi>ul>li>a{\n"
+			    str +="		display:none;\n"
+			    str +="	}\n\n"
+				str +="	#swimbi>ul:hover>li>a, .hvr>li>a{\n"
+				str +="		display:block !important;\n"
+			    str +="	}\n\n"
+				
+				str +="	#swimbi>ul>li:hover, .hvr{\n"
+				str +="		z-index: 9 !important;\n"
+			    str +="	}\n\n"
+				
+			 	str += getHamburgerTextCode()
+
+			 	str +="	#swimbi>ul:before {\n"
+			    str +="		float: right;\n"
+
+			    str +="		line-height: "+menuSettings.mainmenuHeight+"px;\n"
+			   	str +="		content: '';\n"
+			   	str +="		margin: "+(topMargin)+"px 22px "+bottomMargin+"px 0;\n"
+			    str +="		width: "+hamburgerWidth+"px;\n"
+			    str +="		height:"+hamburgerHeight+"px;\n"
+			    str +="		border-top: "+borderTopSize+"px double #"+menuSettings.mainTextColor+";\n"
+			    str +="		border-bottom: "+borderBottomSize+"px solid #"+menuSettings.mainTextColor+";\n"
+			    str +="	}\n\n"
+				
+				str +="	#swimbi>ul:hover:before, #swimbi>ul.hvr:before  {\n"
+			    str +="		opacity: 0;\n"
+			    str +="	}\n\n"
+				
+				str +="	#swimbi ul ul {\n"
+				str +="		left: 25px !important;\n"
+				str +="	}\n\n"
+				str +="	#swimbi ul ul ul{\n"
+				str +="		margin-top: 25px;\n"
+				str +="	}\n"
+				str += "}\n"
+			return str;
+		}else{
+				var str = "@media (max-width: "+menuSettings.responsiveThreshold+"px) {\n"
+				str +="	#swimbi-vertical ul ul {\n"
+				str +="		left: 25px !important;\n"
+				str +="	}\n\n"
+				str +="	#swimbi-vertical ul ul{\n"
+				str +="		margin-top: 25px;\n"
+				str +="	}\n"
+				str += "}\n"
+			return str;
+		}
+	}
+	return '';
+}
